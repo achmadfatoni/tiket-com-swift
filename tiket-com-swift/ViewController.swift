45b69d5a5580acdaa668654ee9ab7233b3b55b1b
Achmad Fatoni <achmadfatony@gmail.com>
2015-09-14 06:52:38 +0800

    initial commit tiket-com-swift

---
+++ tiket-com-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tiket-com-swift
+//
+//  Created by Achmad Fatoni on 9/14/15.
+//  Copyright (c) 2015 Achmad Fatoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
